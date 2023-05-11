83ed604b7b0358a6bfb0884f84b59b93fce9515f
francoonesto <francoonesto2001@gmail.com>
2023-05-10 21:07:40 -0300

    arreglo

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,7 +4,6 @@ const CartWidget = () => {
   return (
     <button type="button" className="btn btn-light position-relative m-3">
       <img src={Icon} alt={"ICON"} width={24} />
-      Inbox
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark">
         1
       </span>
