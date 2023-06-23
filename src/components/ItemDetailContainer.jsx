bf12c0925909ea05b486bd7595c4fe061e8559f6
francoonesto <francoonesto2001@gmail.com>
2023-06-23 14:44:19 -0300

    cambios realizados

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from "react";
-import Productos from "./json/productos.json";
+//import Productos from "./json/productos.json";
 import ItemDetail from "./ItemDetail";
 import { useParams } from "react-router-dom";
+import { doc , getDoc, getFirestore } from "firebase/firestore";
 
 const ItemDetailConteiner = () => {
     const [item , setItem] = useState({});
     const {id} = useParams();
 
-    useEffect(()=>{
-        const promesa = new Promise((resolve) => {
-            setTimeout(() => {
-resolve(Productos.find(item => item.id === parseInt(id)));
+//     useEffect(()=>{
+//         const promesa = new Promise((resolve) => {
+//             setTimeout(() => {
+// resolve(Productos.find(item => item.id === parseInt(id)));
 
-            },2000)
-        })
-        promesa.then (data => {
-            setItem(data);
-        })
-    },[id]);
+//             },2000)
+//         })
+//         promesa.then (data => {
+//             setItem(data);
+//         })
+//     },[id]);
+
+useEffect(() => {
+const db = getFirestore();
+const producto = doc(db , "Mercaderia" , id);
+getDoc(producto).then(resultado =>{
+    setItem({id:resultado.id,...resultado.data()})
+});
+},[id]);
 
     return(
         <>
